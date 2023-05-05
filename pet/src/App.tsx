98d210f6075bfff3f5cfe28f1cc00eceb5594df7
bengyh <115003549+bengyh@users.noreply.github.com>
2023-05-06 01:31:41 +0800

    create base files

---
+++ pet/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header>P.E.T</header>
+      <header>Pets for Everyone Today</header>
+      <main></main>
+      <footer>2023 PET</footer>
+    </div>
+  )
+}
+
+export default App
