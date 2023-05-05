028cd62e81b67e9215e60bd80d592024d56267e8
bengyh <115003549+bengyh@users.noreply.github.com>
2023-05-06 05:59:59 +0800

    linking of product to api folder

--- pet/src/main.tsx
+++ pet/src/main.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './App';
 import './index.css';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
+import axios from 'axios';
+
+//prevent creation of any url in localhost 4000 which is the same as backend host
+axios.defaults.baseURL =
+  process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
