6565404502831e8ac96bbb48355763e1261d869d
bengyh <115003549+bengyh@users.noreply.github.com>
2023-05-11 21:54:42 +0800

    user sign in, sign in page and register new user

--- pet/src/main.tsx
+++ pet/src/main.tsx
@@ -6,17 +6,18 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
-//import 'bootstrap/dist/css/bootstrap.min.css';
 import { HelmetProvider } from 'react-helmet-async';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from './App';
 import './index.css';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { StoreProvider } from './Store';
 import CartPage from './pages/CartPage';
+import SigninPage from './pages/SigninPage';
+import SignupPage from './pages/SignupPage';
 
 const queryClient = new QueryClient();
 
@@ -26,6 +27,8 @@ const router = createBrowserRouter(
       <Route index={true} element={<HomePage />} />
       <Route path="product/:slug" element={<ProductPage />} />
       <Route path="cart" element={<CartPage />} />
+      <Route path="signin" element={<SigninPage />} />
+      <Route path="signup" element={<SignupPage />} />
     </Route>
   )
 );
